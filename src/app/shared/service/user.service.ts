a2e34ced156c34321d0629478904bc378924e7d4
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2024-09-12 18:02:09 -0300

    Like Function

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -52,6 +52,7 @@ export class UserService {
     }
     this.like.id = postId;
     this.like.type = type;
+    console.log(this.like)
     return this.http.post(this.userUrl + `post/${this.getUserId()}`, this.like);
   }
 
