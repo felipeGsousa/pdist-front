ff273fca628f463b5894e4a5892ca09ba460b4ad
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2024-09-13 02:41:04 -0300

    Some visual changes

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -27,6 +27,7 @@ export class PostListComponent implements OnInit {
   ngOnInit(): void {
     this.loggedIn = this.userService.isLoggedIn();
     this.user = this.userService.getUser();
+    console.log(this.user);
     this.loadPosts();
     //this.loadFile();
   }
