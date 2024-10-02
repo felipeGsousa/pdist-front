f3a899a5df1dfba1a8193a1d2c6976421f1739cf
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2024-10-02 00:41:46 -0300

    some changes

--- src/app/forum/forum-details/forum-details.component.ts
+++ src/app/forum/forum-details/forum-details.component.ts
@@ -52,7 +52,6 @@ export class ForumDetailsComponent implements OnInit {
         }
         if (this.loggedIn) {
           const user = this.userService.getUser();
-          console.log(this.forumDTO?.users)
           if (this.forumDTO?.users.find((obj)=> obj === user.id) || this.forumDTO?.userId === user.id) {
             this.userIn = true;
           }
