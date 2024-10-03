3791bfbfe4ca78b25d62dbe39005f88764a09c9e
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2024-10-03 14:35:24 -0300

    Last Commit

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -13,7 +13,7 @@ import { UserService } from 'src/app/shared/service/user.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public dialog: MatDialog, private sanitizer: DomSanitizer, private route:ActivatedRoute, private userService:UserService, private postService: PostService) { }
+  constructor(public dialog: MatDialog, private sanitizer: DomSanitizer, private route:ActivatedRoute, private userService:UserService) { }
 
   posts: any[] = [];
   loggedIn:boolean = false;
@@ -22,7 +22,6 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.userService.getUser();
     this.loggedIn = this.userService.isLoggedIn();
-    this.loadRecentPosts();
   }
 
   createForum(): void {
@@ -35,37 +34,4 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  loadRecentPosts(): void {
-    this.postService.getRecentPosts(this.userService.getUserId()).subscribe((response: any) => {
-      this.posts = response.data.getRecentPosts;
-      this.posts.forEach( post => {
-        if (post.file != undefined) {
-          if (post.file.data != null) {
-            const byteCharacters = atob(post.file?.data);
-            const byteNumbers = new Array(byteCharacters.length)
-    
-            for (let i = 0; i < byteCharacters.length; i++) {
-              byteNumbers[i] = byteCharacters.charCodeAt(i);
-            }
-
-            const byteArray = new Uint8Array(byteNumbers);
-    
-            const blob = new Blob([byteArray], { type: post.file.contentType });
-            
-            const url = URL.createObjectURL(blob);
-            post.fileType = post.file.contentType;
-            post.fileUrl = this.sanitizeUrl(url);
-            post.fileName = post.file.filename;
-            
-          }
-        }
-      })
-    }, (error) => {
-      console.error('Error fetching posts:', error);
-    });
-  }
-
-  sanitizeUrl(fileString: string): SafeUrl{
-    return this.sanitizer.bypassSecurityTrustUrl(fileString);
-  }
 }
